ee85b99535afde9d6694f126c365e256471fb4c0
Jefferson Sena <jefferson.senna777@gmail.com>
2023-08-15 20:04:06 -0300

    testar no celular

---
+++ script/produto.js
@@ -0,0 +1,26 @@
+function abrirWhey(){
+    neon2_whey.style.display == 'inline'
+    neon2_cre.style.display = 'none'
+    neon2_pre.style.display = 'none'
+    neon2_hip.style.display = 'none' 
+    iframe.style.display = 'block' 
+    neon2_whey.style.display = 'none'
+}
+function abrirCre(){
+  neon2_cre.style.display == 'inline'
+  neon2_whey.style.display = 'none'
+  neon2_pre.style.display = 'none'
+  neon2_hip.style.display = 'none'  
+}
+function abrirPre(){
+  neon2_pre.style.display == 'inline'
+  neon2_cre.style.display = 'none'
+  neon2_whey.style.display = 'none'
+  neon2_hip.style.display = 'none'  
+}
+function abrirHip(){
+  neon2_hip.style.display == 'inline'
+  neon2_cre.style.display = 'none'
+  neon2_pre.style.display = 'none'
+  neon2_whey.style.display = 'none'  
+}
