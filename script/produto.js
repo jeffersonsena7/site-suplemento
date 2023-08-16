e4683066862b494633dd6ec093edb8c5a52ab2ab
Jefferson Sena <jefferson.senna777@gmail.com>
2023-08-16 20:46:12 -0300

    testando todas as paginas

--- script/produto.js
+++ script/produto.js
@@ -3,26 +3,33 @@ function abrirWhey(){
     neon2_cre.style.display = 'none'
     neon2_pre.style.display = 'none'
     neon2_hip.style.display = 'none'
+    completo.style.display = 'none'
     iframe.style.display = 'block' 
-    rodape.style.display = 'none'
-    logo.style.display = 'none'
+    
+    
       
 }
 function abrirCre(){
   neon2_cre.style.display == 'inline'
   neon2_whey.style.display = 'none'
   neon2_pre.style.display = 'none'
-  neon2_hip.style.display = 'none'  
+  neon2_hip.style.display = 'none'
+  completo.style.display = 'none'
+  iframe2.style.display = 'block'  
 }
 function abrirPre(){
   neon2_pre.style.display == 'inline'
   neon2_cre.style.display = 'none'
   neon2_whey.style.display = 'none'
-  neon2_hip.style.display = 'none'  
+  neon2_hip.style.display = 'none' 
+  completo.style.display = 'none'
+  iframe3.style.display = 'block'  
 }
 function abrirHip(){
   neon2_hip.style.display == 'inline'
   neon2_cre.style.display = 'none'
   neon2_pre.style.display = 'none'
-  neon2_whey.style.display = 'none'  
+  neon2_whey.style.display = 'none'
+  completo.style.display = 'none'
+  iframe4.style.display = 'block'   
 }
